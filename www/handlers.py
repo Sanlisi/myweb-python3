9eaedbd638a2a010d666b5aa1538aafddd4730b0
sanlisi <2740848228.qq.com>
2019-05-16 15:28:35 +0100

    day8: add css && js, but test not full success

--- www/handlers.py
+++ www/handlers.py
@@ -1,13 +1,21 @@
 #!/bin/usr/env python3
 #-*- coding: utf-8 -*-
 
+import time
+
 from coroweb import get, post
 from models import User, Comment, Blog
 
 @get('/')
-async def index(request):
+async def logs(request):
+    summary = 'I am test output, linke hello world...'
+    blogs = [
+            Blog(id='1', name='Test01', summary=summary, create_at=time.time()-120),
+            Blog(id='2', name='Test02', summary=summary, create_at=time.time()-3600),
+            Blog(id='3', name='Test03', summary=summary, create_at=time.time()-7201)
+            ]
     users = await User.findAll()
     return {
-            '__template__': 'test.html',
-            'users': users
+            '__template__': 'blogs.html',
+            'blogs': blogs 
     }
