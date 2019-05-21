d665828c79c09d99f4353b4e4c75baa643e49d06
sanlisi <2740848228.qq.com>
2019-05-21 16:29:40 +0100

    day9: day8 test success & add users api

--- www/handlers.py
+++ www/handlers.py
@@ -14,8 +14,14 @@ async def logs(request):
             Blog(id='2', name='Test02', summary=summary, create_at=time.time()-3600),
             Blog(id='3', name='Test03', summary=summary, create_at=time.time()-7201)
             ]
-    users = await User.findAll()
     return {
             '__template__': 'blogs.html',
             'blogs': blogs 
     }
+
+@get('/api/users')
+async def api_get_users(request):
+    users = await User.findAll(orderBy='create_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
